7ba10a607f8cd5cd9c2637b5280dc9dd1e139ea7
Rodney Kimathi <rodneykimathi@gmail.com>
2022-10-26 17:42:47 +0300

    Update readme

--- millionaire.py
+++ millionaire.py
@@ -1,6 +1,6 @@
 from components.play import play_game
 
-print("Hello! Welcome to 'Who Wants to Be a Millionaire'.")
+print("Hello! Welcome to 'Who Wants to Be a Millionaire?'.")
 print("I am your host, Rodney Kimathi. We have a special guest joining us.\n")
 
 name = input("What is your name? ")
@@ -11,5 +11,5 @@ print("The rules are simple, answer questions and win money. Let us begin!")
 
 prize_money = play_game()
 
-print("\nThank you {} for playing 'Who Wants to Be a Millionaire'. Your total prize money is ${}.".format(name, prize_money))
+print("\nThank you {} for playing 'Who Wants to Be a Millionaire?'. Your total prize money is ${}.".format(name, prize_money))
 print("See you next time!")
