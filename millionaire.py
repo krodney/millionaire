4054c2bafcb959574d070f504026ab74f713855e
Rodney Kimathi <rodneykimathi@gmail.com>
2022-10-27 12:14:55 +0300

    Add banner

--- millionaire.py
+++ millionaire.py
@@ -1,5 +1,15 @@
 from components.play import play_game
 
+print("""
+M     M   I   L         L         I    OOOOO    N     N      A      I   RRRRRR    EEEEEEE
+M M M M   I   L         L         I   O     O   N N   N      A      I   R     R   E      
+M  M  M   I   L         L         I   O     O   N  N  N     A A     I   RRRRRR    EEEEEEE
+M     M   I   L         L         I   O     O   N   N N    AAAAA    I   R  R      E      
+M     M   I   LLLLLLL   LLLLLLL   I    OOOOO    N     N   A     A   I   R     R   EEEEEEE
+""")
+print("""
+-----------------------------------------------------------------------------------------\n
+""")
 print("Hello! Welcome to 'Who Wants to Be a Millionaire?'.")
 print("I am your host, Rodney Kimathi. We have a special guest joining us.\n")
 
